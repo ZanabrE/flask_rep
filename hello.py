f0f932cc937fbf4d3ec06f1e26cc4cf757aa424c
G:\Git <ernesto.zanabria1979@gmail.com>
2024-03-16 14:19:39 -0400

    modified: hello.py

--- hello.py
+++ hello.py
@@ -1,18 +1,25 @@
 from flask import Flask
 from flask import request
 from flask import make_response
+from flask import redirect
+from flask import abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    response = make_response('<h1>This document carries a cookies!</h1>')
-    response.set_cookie('answer', '42')
-    return response
+    return redirect('http://www.example.com')
 
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
+
+@app.route('/user/<id>')
+def get_user(id):
+    user = load_user(id)
+    if not user:
+        abort(404)
+    return '<h1>Hello, %s</h1>' % user.name
 
 if __name__ == '__main__':
     app.run(debug=True)
