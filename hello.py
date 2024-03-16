b5002b220fd43d95f20fd63c3e420cd5496ffa5f
G:\Git <ernesto.zanabria1979@gmail.com>
2024-03-16 15:25:50 -0400

    modified: hello.py

--- hello.py
+++ hello.py
@@ -3,10 +3,11 @@ from flask import request
 from flask import make_response
 from flask import redirect
 from flask import abort
-from flask.exe.bootstrap import Bootstrap
+from flask.ext.bootstrap import Bootstrap
+
+bootstrap = Bootstrap(app)
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
